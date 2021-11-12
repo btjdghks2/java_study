46d758fc948a7d27279e67da5f9620174ed12450
sung <btjdghks2@gmail.com>
2021-11-12 13:30:34 +0900

    class

---
+++ src/test_pro_first/MemberService.java
@@ -0,0 +1,20 @@
+package test_pro_first;
+
+public class MemberService{
+
+Member mamber = new Member();
+
+boolean login(String id,String password){
+if("hong".equals(id) && "12345".equals(password))
+return true;
+else
+return false;
+
+}
+void logout(String id){
+  System.out.println("로그아웃 되었습니다");
+
+}
+
+  
+}
