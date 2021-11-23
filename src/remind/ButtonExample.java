5199e8cdc3b8aad8cbe2857a60e4b84de6511bbf
sung <btjdghks2@gmail.com>
2021-11-24 04:06:00 +0900

    중첩　interface

---
+++ src/remind/ButtonExample.java
@@ -0,0 +1,17 @@
+package remind;
+
+public class ButtonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Button btn = new Button();
+		
+		btn.setOnClickListener(new CallListener());
+		btn.touch();
+		
+		btn.setOnClickListener(new MessageListener());
+		btn.touch();
+
+	}
+
+}
---
+++ src/remind/CallListener.java
@@ -0,0 +1,13 @@
+package remind;
+
+public class CallListener implements Button.OnClickListener {
+
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		System.out.println("전화를　켭니다");
+		
+		
+	}
+
+}
