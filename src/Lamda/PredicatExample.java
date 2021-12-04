a5c8ce33c971ccf7a5bfbaca51ad0ec2aaffc458
sung <btjdghks2@gmail.com>
2021-12-05 00:43:11 +0900

    lamda api

---
+++ src/Lamda/PredicatExample.java
@@ -0,0 +1,21 @@
+package Lamda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PredicatExample {
+
+	public static void main(String[] args) {
+
+		private static List<Student> list = Arrays.asList(
+				new Student("홍길동","남자",90),
+				new Student("김순희","여자",90),
+				new Student("김자바","남자",95),
+				new Student("박한나","여자",90)
+				
+				);
+		
+		
+	}
+
+}
