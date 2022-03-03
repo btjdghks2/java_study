27e43f6e0e4b4f11b3f67db7ea67a268624f351e
sung <btjdghks2@gmail.com>
2022-03-03 18:48:52 +0900

    double checked locking

--- src/singleton/Settings.java
+++ src/singleton/Settings.java
@@ -2,14 +2,21 @@ package singleton;
 
 public class Settings {
 	
-	private static final Settings INSTANCE = new Settings();
+	private static Settings instance;
 	
 	private Settings() {}
 	
-	public static Settings getInstance() {
-
+	public static synchronized Settings getInstance() {
+		if (instance == null) {
+			synchronized (Settings.class) {
+				if (instance == null) {
+					instance = new Settings();	
+				}
+			}
 		
-		return INSTANCE;
+		}
+		
+		return instance;
 	}
 
 }
